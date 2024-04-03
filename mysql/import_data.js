b77fddfb5ef1f462271a6515bfa527d71fe80a6c
Bob Hua <schua1013@gmail.com>
2024-04-02 21:24:40 -0400

    add progress bar

--- mysql/import_data.js
+++ mysql/import_data.js
@@ -1,35 +1,39 @@
 const { createReadStream } = require('fs')
-const { ensureTablesExist } = require('./utils')
+const { ensureTablesExist, importData, linkTables } = require('./utils')
 const { pipeline } = require('stream/promises')
-const { insertData } = require('./transforms')
 const { dataFilePaths, getTransforms } = require('./file_maps')
-const pool = require('./db')
+const { pool } = require('./db')
+const cliProgress = require('cli-progress')
 
 const dbTablesMaps = {
   Locations: 'Locations.sql',
   AdjacencyRegions: 'AdjacencyRegions.sql'
 }
-
 async function processFiles () {
+  const multibar = new cliProgress.MultiBar(
+    { format: 'Importing {file} [{bar}] {percentage}% | {value}/{total} | Resolved: {resolved} | Errors: {errors}' },
+    cliProgress.Presets.shades_classic
+  )
   await Promise.all(dataFilePaths.map(async (dataFilePath) => {
     const transforms = getTransforms(dataFilePath)
     await pipeline(
       createReadStream(dataFilePath, { encoding: 'utf-8' }),
       ...transforms,
-      insertData
+      importData(multibar, dataFilePath)
     )
   }))
+  multibar.stop()
 }
 
 async function main () {
   try {
     await ensureTablesExist(dbTablesMaps)
+    // await linkTables()
     await processFiles()
   } catch (error) {
     console.error(error)
   } finally {
     await pool.end()
-    console.log('Pool End')
   }
 }
 main()
--- mysql/utils.js
+++ mysql/utils.js
@@ -1,15 +1,61 @@
 const { readFile } = require('fs/promises')
 const path = require('path')
-const pool = require('./db')
+const { pool } = require('./db')
+const cliProgress = require('cli-progress')
 
 const sqlRoot = path.join(__dirname, 'sql')
+
+const importData = (multibar, filePath) => {
+  const queries = []
+  const barStatus = {
+    resolved: 0,
+    errors: 0,
+    file: path.basename(filePath)
+  }
+  const bar = multibar.create(queries.length, barStatus.errors + barStatus.resolved, barStatus)
+  return (stream) => {
+    stream.on('data', (chunk) => {
+      const [sql, values] = chunk
+      queries.push(
+        pool.execute(sql, values)
+          .then(() => bar.update(null, { resolved: ++barStatus.resolved }))
+          .catch((err) => {
+            console.error(err)
+            bar.update(null, { errors: ++barStatus.errors })
+          })
+          .finally(() => bar.increment())
+      )
+      bar.setTotal(queries.length)
+    })
+    stream.on('error', (error) => {
+      throw error
+    })
+    return new Promise((resolve) => {
+      stream.on('end', async () => {
+        await Promise.all(queries)
+        resolve()
+      })
+    })
+  }
+}
+const linkTables = async () => {
+  const sql = await readFile(path.join(sqlRoot, 'alters.sql'), { flag: 'r', encoding: 'utf-8' })
+  await pool.execute(sql)
+}
 /**
  *
  * @param {Object} tableMaps
  */
 async function ensureTablesExist (tableMaps) {
+  const bar = new cliProgress.SingleBar({
+    format: 'Checking Tables [{bar}] {percentage}% | {value}/{total}'
+  }, cliProgress.Presets.shades_classic)
+  bar.start(Object.keys(tableMaps).length, 0)
   try {
-    await Promise.all(Object.entries(tableMaps).map(tableEntry => createTable(tableEntry)))
+    await Promise.all(Object.entries(tableMaps).map((tableEntry, _, arr) =>
+      createTable(tableEntry).finally(() => bar.increment())
+    ))
+    bar.stop()
   } catch (error) {
     console.error(error)
   }
@@ -21,4 +67,4 @@ async function createTable (tableEntry) {
     await pool.execute(sql)
   }
 }
-module.exports = { ensureTablesExist }
+module.exports = { ensureTablesExist, importData, linkTables }
