8d4345d97af059eada6b5522006fc79f0cfa2f9f
Jimei Fang <fjmabc00@126.com>
2024-04-07 22:52:27 -0400

    update Q1 frontend and backend

--- routers/mysql.js
+++ routers/mysql.js
@@ -8,4 +8,26 @@ router.get('/', (req, res) => {
   res.json('This is mysql api')
 })
 
+router.get('/forecast', async (req, res) => {
+  const { zipcode, datetime } = req.query;
+ console.log(datetime);
+  if (!zipcode || !datetime) {
+      return res.status(400).json({ message: "Please provide both zipcode and datetime." });
+  }
+
+
+  try {
+      const [results] = await pool.execute(Q1.sql, [zipcode, datetime]);
+      if (results.length === 0) {
+          return res.status(404).json({ message: "No forecast data found." });
+      }
+      res.json(results);
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Error fetching forecast data." });
+  }
+});
+
+
+
 module.exports = router
